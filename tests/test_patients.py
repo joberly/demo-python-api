ecccd869c36a265e0c9398b11532b788d9ff503a
John Oberly III <joberly@gmail.com>
2024-10-05 18:29:22 -0500

    test get single patient by id

--- tests/test_patients.py
+++ tests/test_patients.py
@@ -1,5 +1,4 @@
-from fastapi.testclient import TestClient
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 import pytest
 
 from api import app
@@ -9,17 +8,19 @@ from model import Patient
 def test_setup_db():
     Patient.delete().execute()
 
+# Get empty patients list
 @pytest.mark.asyncio
 async def test_get_patients_empty():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/patients/")
         assert response.status_code == 200
         assert response.json() == []
 
+# Get single patient in list
 @pytest.mark.asyncio
 async def test_get_patients():
     patient = Patient.create(first_name="Pat", last_name="Doe")
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         response = await ac.get("/patients/")
         assert response.status_code == 200
         assert response.json() == [
@@ -29,3 +30,16 @@ async def test_get_patients():
                 "last_name": "Doe",
             }
        ]
+
+# Get single patient by ID
+@pytest.mark.asyncio
+async def test_get_patient():
+    patient = Patient.create(first_name="Pat", last_name="Doe")
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        response = await ac.get(f"/patients/{patient.id}")
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": str(patient.id),
+            "first_name": "Pat",
+            "last_name": "Doe",
+        }
