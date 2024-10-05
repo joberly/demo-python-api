58a9a4a8fa9c0300999a22699d0a2e9f8a376237
John Oberly III <joberly@gmail.com>
2024-10-05 13:13:40 -0500

    add data model

---
+++ api/model.py
@@ -0,0 +1,36 @@
+from peewee import Model, CharField, ForeignKeyField, DateField, IntegerField, SqliteDatabase
+from db_config import db
+
+
+# Patient data model
+class Patient(Model):
+    first_name = CharField()
+    last_name = CharField()
+
+    class Meta:
+        database = db
+
+# Current Procedural Terminology (CPT) code model
+class CPTCode(Model):
+    code = CharField(unique=True)
+    description = CharField()
+
+    class Meta:
+        database = db
+
+# Encounter data model
+class Encounter(Model):
+    patient = ForeignKeyField(Patient, backref='encounters')
+    date = DateField()
+
+    class Meta:
+        database = db
+
+# Line item data model
+class LineItem(Model):
+    encounter = ForeignKeyField(Encounter, backref='line_items')
+    cpt_code = ForeignKeyField(CPTCode)
+    units = IntegerField()
+
+    class Meta:
+        database = db
